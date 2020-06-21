fd6909d9174681ed95f472a29d1944e087e77b8a
guoyanwu11115 <guoyanwu11115@163.com>
2020-06-21 22:42:13 +0800

    clean commit

--- src/main/java/com/miphalink/unity/project/system/user/dao/MenuMapper.java
+++ src/main/java/com/miphalink/unity/project/system/user/dao/MenuMapper.java
@@ -5,17 +5,17 @@ import com.miphalink.unity.project.system.user.domain.Menu;
 import java.util.List;
 
 public interface MenuMapper {
-    List<Menu> selectMenuNormalAll();
-
     int deleteByPrimaryKey(Integer menuId);
-
-    Menu selectByPrimaryKey(Integer menuId);
 
     int insert(Menu record);
 
     int insertSelective(Menu record);
 
+    Menu selectByPrimaryKey(Integer menuId);
+
     int updateByPrimaryKeySelective(Menu record);
 
     int updateByPrimaryKey(Menu record);
+
+    List<Menu> selectMenuNormalAll();
 }
